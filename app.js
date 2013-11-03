72a01489a5d3700d91e6e5929d5f19470eaac083
Jens-Ole Graulund <jensole@graulund.net>
2013-11-03 19:34:23 +0100

    Change pusher account

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.enable("jsonp callback", true);
 
 app.use(express.logger());
 
-var pusher = new Pusher( { appId: 53733, key: 'bc34941c47e130e926a6', secret:'fc3815584f420524a234' } );
+//var pusher = new Pusher( { appId: 53733, key: 'bc34941c47e130e926a6', secret:'fc3815584f420524a234' } );
+var pusher = new Pusher( { appId: 52768, key: '0449a107cdc44df52703', secret:'3e9154bccc7d45ef3c27' } );
+
 
 app.get('/', function(req, res) {
     res.send('Pusher auth server running ... ');
